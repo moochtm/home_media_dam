ec37dd96350cedb4eb786d3e558aa5656f9d5e7a
home <moochtm@hotmail.com>
2019-10-18 22:21:47 +0100

    added support for optparseto, multiple target paths in list

--- script_set_created_date_to_match_exif.py
+++ script_set_created_date_to_match_exif.py
@@ -2,11 +2,13 @@
 # import (1) python built-in, (2) downloaded 3rd party, (3) own
 
 import time, os
+from optparse import OptionParser
 import inspect
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
 import src.thirdparty.exiftool as exiftool
+et = exiftool.ExifTool()
 import src.thirdparty.sortphotos as sortphotos
 
 import settings
@@ -49,18 +51,28 @@ logging.getLogger('').addHandler(file_log)
 
 log = logging.getLogger(__name__)
 
-def main(root_path, dummy_run):
+def main(args, dummy_run):
 
     print '----------------------------------------------------------------'
     print 'starting up exiftool'
 
-    et = exiftool.ExifTool()
     et.start()
 
-    print 'starting to walk from root (%s)' % root_path
+    for arg in args:
+        process(arg, dummy_run)
+
+    print 'closing exiftool'
+
+    et.terminate()
+
+    print '----------------------------------------------------------------'
+
+
+def process(root_path, dummy_run):
+    print 'starting to process (%s)' % root_path
 
     if not os.path.isdir(root_path):
-        print 'ERROR root is not folder (%s)' % root_path
+        print 'ERROR path is not a folder (%s)' % root_path
         return False
 
     for dirName, subdirList, fileList in os.walk(root_path):
@@ -71,23 +83,21 @@ def main(root_path, dummy_run):
             print 'file: %s' % file_path
             file_exif = et.get_metadata(file_path)
             _, oldest_date, oldest_date_tag = sortphotos.get_oldest_timestamp(file_exif, ['File'], [])
+            print oldest_date_tag
             if oldest_date is not None:
                 oldest_time = time.mktime(oldest_date.timetuple())
                 print "oldest date: %s (tag: %s)" % (oldest_date, oldest_date_tag)
                 print "writing to file's create date..."
                 if not dummy_run:
-                        os.utime(file_path, (oldest_time, oldest_time))
-
-    print 'closing exiftool'
-
-    et.terminate()
-
-    print '----------------------------------------------------------------'
+                    os.utime(file_path, (oldest_time, oldest_time))
 
 
 if __name__ == "__main__":
 
-    root_path = '/Users/home/Pictures/ORIGINALS/Google Photos Backup'
+    parser = OptionParser()
+    parser.add_option("-d", "--dummy_run")
 
-    main(root_path, dummy_run=True)
+    (options, args) = parser.parse_args()
+
+    main(args, dummy_run=options.dummy)
 
